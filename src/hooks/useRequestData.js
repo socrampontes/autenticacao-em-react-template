67d48c9302365110adef0c4fee9aff63eac7b232
C.Meirelles <clara_meirelles@hotmail.com>
2023-02-28 13:10:48 -0300

    tirei o headers pra lembrar de mencionar em aula

--- src/hooks/useRequestData.js
+++ src/hooks/useRequestData.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-export default function useRequestData(estadoInicial, path, headers) {
+export default function useRequestData(estadoInicial, path) {
     const [dados, setDados] = useState(estadoInicial)
     const recebeDados = () => {
 
         const aluno = 'darvas'//coloque aqui o nome que você criou no começo da aula
-        axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labeX/${aluno}${path}`, headers)
+        axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labeX/${aluno}${path}`)
           .then((res) => {
             setDados(res.data.trips)
           })
