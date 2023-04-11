42c3563d618543823bd721790e36307af105c07a
yuzo <yuzokamoto@gmail.com>
2023-04-11 15:46:18 -0300

    update

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,29 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
+import useForms from '../../hooks/useForms'
 import { ContainerForm, ContainerSignup, Input } from './styled'
 
 export default function Signup() {
-    const [nomeUsuario, setNomeUsuario] = useState("")
-    const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [confirmaSenha, setConfirmaSenha] = useState("")
+    const { form, onChange } = useForms({ email: "", senha: "", nomeUsuario: "", confirmaSenha: "" })
 
-    const onChangeNome = (e) => {
-        setNomeUsuario(e.target.value)
-    }
-    const onChangeEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    const onChangeSenha = (e) => {
-        setSenha(e.target.value)
-    }
-    const onChangeConfirmaSenha = (e) => {
-        setConfirmaSenha(e.target.value)
-    }
-
-    const enviarCadastro = () => {
+    const enviarCadastro = (e) => {
+        e.preventDefault()
         //* EXTRA: validando a senha - ter certeza que o usuário sabe qual senha cadastrou
-        if (senha === confirmaSenha) {
-            console.log({nomeUsuario, email, senha, confirmaSenha})
+        // não é necessário caso use o pattern para a mesma funcionalidade
+        if (form.senha === form.confirmaSenha) {
+            const dadosUsuario = {
+                username: form.nomeUsuario,
+                email: form.email,
+                password: form.senha
+            }
+            console.log(dadosUsuario)
+        } else {
+            alert("Digite a mesma senha nos campos 'senha' e 'confirmação de senha'")
         }
     }
 
@@ -33,30 +27,47 @@ export default function Signup() {
                 <label htmlFor='nome'>Nome de usuario:</label>
                 <Input
                     id='nome'
-                    value={nomeUsuario}
-                    onChange={onChangeNome}
+                    name='nomeUsuario'
+                    pattern='[a-zA-Z\u00C0-\u00FF ]{3,}'
+                    title='Nome de usuário deve ter no mínimo 3 caracteres. Podendo conter letras, acentos e espaço'
+                    value={form.nomeUsuario}
+                    onChange={onChange}
                     placeholder="username"
+                    required
                 />
                 <label htmlFor='email'>Email:</label>
                 <Input
                     id='email'
-                    value={email}
-                    onChange={onChangeEmail}
+                    name='email'
+                    type={"email"}
+                    value={form.email}
+                    onChange={onChange}
                     placeholder="nome@email.com"
+                    required
                 />
                 <label htmlFor='senha'>Senha:</label>
                 <Input
                     id='senha'
-                    value={senha}
-                    onChange={onChangeSenha}
+                    name='senha'
+                    type="password"
+                    minLength={8}
+                    value={form.senha}
+                    onChange={onChange}
                     placeholder="Crie sua senha"
+                    required
                 />
                 <label htmlFor='confirma-senha'>Confirmação de senha:</label>
-                <Input
-                    id='confirma-senha'
-                    value={confirmaSenha}
-                    onChange={onChangeConfirmaSenha}
-                    placeholder="Confirme a senha"
+                <Input 
+                id='confirma-senha' 
+                name='confirmaSenha' 
+                type={"password"}
+                value={form.confirmaSenha} 
+                onChange={onChange} 
+                placeholder="Confirme a senha"
+                // verifica se a senha é a mesma nos dois campos
+                pattern={`${form.senha}`} 
+                title="confirme a senha digitada"
+                required
                 />
                 <button>Cadastrar</button>
             </ContainerForm>
